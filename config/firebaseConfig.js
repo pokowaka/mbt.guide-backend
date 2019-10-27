9fa69dba3b102153e0806ff708c08e232f1d1924
Justin Headley <jheadley@archsys.io>
2019-10-27 01:23:03 -0700

    feature: firebase config

---
+++ config/firebaseConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyAn6loR5s_OC4aqA-nlMfpwOH2BogTM79g',
+  authDomain: 'mbt-guide-d9b1b.firebaseapp.com',
+  databaseURL: 'https://mbt-guide-d9b1b.firebaseio.com',
+  projectId: 'mbt-guide-d9b1b',
+  storageBucket: 'gs://mbt-guide-d9b1b.appspot.com/',
+  messagingSenderId: '461006931750',
+  appId: '1:461006931750:web:3c39f2584c45ac7c',
+};
